a03f6062bda94486c202942939dc3ce8cb5c42b4
YaaseenK <yaaseenkhan02@gmail.com>
2022-10-19 16:46:22 -0400

    color style update // working search .. displaying information

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,13 +1,44 @@
 const APIkey = '713127e45178ba0f5a1e551fd77a5b80';
 
-var city = 'austin';
+// $(`#location`).val;
 
-var reqURL =  "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+const temp = $('#temp');
+const wind = $('#wind');
+const humidity = $('#humidity');
+const cityNameTitle = $('#cityTitle')
 
-    fetch(reqURL) 
-    .then((res) => {
-        return res.json();
-    }) 
-    .then((res) => {
-        console.log(res)
-    });
+$('#search').on('click' , () =>{
+    let city = $('#location').val();
+    cityNameTitle.text(`${city}`)
+    const reqURL =  "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+    if(city = null){
+        console.log('err');
+    }
+        fetch(reqURL) 
+        .then((res) => {
+            return res.json();
+        })
+        .then((data)=>{
+            console.log(data)
+            let weatherData = {
+                    temp: data.main.temp,
+                    wind: data.wind.speed,
+                    humidity: data.main.humidity
+                }
+                render(weatherData)
+        })
+        .catch(err => console.error(err));
+})
+
+    function render(weatherData){
+        
+        let weather = weatherData;
+        let cityTemp = Math.round(weather.temp - 273.15);
+        let cityWind = weather.wind;
+        let cityHumidity = weather.humidity;
+        
+        temp.text(`Temp: ${cityTemp}°C`)
+        wind.text(`Wind: ${cityWind} / Mph`)
+        humidity.text(`Humidity: ${cityHumidity} %`)
+    }
+
